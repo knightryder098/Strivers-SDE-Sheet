35421dba98ac3f04f6c77b01d08691b10aa2f3fb
Gobinda Lugun <42076573+knightryder098@users.noreply.github.com>
2023-06-16 16:01:31 +0530

    Create 184.Palindrome Partitioning ll.cpp

---
+++ 184.Palindrome Partitioning ll.cpp
@@ -0,0 +1,28 @@
+//https://www.codingninjas.com/codestudio/problems/palindrome-partitioning-ll_8230732?challengeSlug=striver-sde-challenge&leftPanelTab=0
+
+
+#include <bits/stdc++.h> 
+int palindromePartitioning(string s) {
+    // Write your code here
+     const int N = s.size();
+            if(N<=1) return 0;
+            int i,j;
+            bool isPalin[N][N];
+            fill_n(&isPalin[0][0], N*N, false);
+            int minCuts[N+1];
+            for(i=0; i<=N; ++i) minCuts[i] = i-1;
+            
+            for(j=1; j<N; ++j)
+            {
+                for(i=j; i>=0; --i)
+                {
+                    if( (s[i] == s[j]) && ( ( j-i < 2 ) || isPalin[i+1][j-1] ) )
+                    {
+                        isPalin[i][j] = true;
+                        minCuts[j+1] = min(minCuts[j+1], 1 + minCuts[i]);    
+                    }
+                }
+            }
+            return minCuts[N];
+            
+}
