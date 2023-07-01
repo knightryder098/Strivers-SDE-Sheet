4e96b40f662bb7ad3d7da7bca7e1c0c42c10cbe9
Gobinda Lugun <42076573+knightryder098@users.noreply.github.com>
2023-07-01 23:27:30 +0530

    Update 65.Median of two sorted arrays.cpp

--- 65.Median of two sorted arrays.cpp
+++ 65.Median of two sorted arrays.cpp
@@ -1,42 +1,55 @@
 // https://www.codingninjas.com/studio/problems/median-of-two-sorted-arrays_8230788?challengeSlug=striver-sde-challenge&leftPanelTab=0
 
-int getK(vector<int>&a,int i1,int j1,vector<int>&b,int i2,int j2,int k){
-	if(j1<i1)return b[i2+k];
-	if(j2<i2)return a[i1+k];
-	if(k==0)return min(a[i1],b[i2]);
 
-	int len1=j1-i1+1;
-	int len2=j2-i2+1;
-	int mid1=(k*len1)/(len1+len2);
-	int mid2=k-mid1-1;
-	mid1+=i1;
-	mid2+=i2;
-	if(a[mid1]<b[mid2]){
-		k=k-(mid1-i1+1);
-		j2=mid2;
-		i1=mid1+1;
-	}
-	else{
-		k=k-(mid2-i2+1);
-		j1=mid1;
-		i2=mid2+1;
-	}
-	return getK(a,i1,j1,b,i2,j2,k);
+int getK(const vector<int>& A, const vector<int>& B, int k, int aStart, int aEnd, int bStart, int bEnd) {
+        // If the segment of on array is empty, it means we have passed all
+        // its element, just return the corresponding element in the other array.
+        if (aEnd < aStart) {
+            return B[k - aStart];
+        }
+        if (bEnd < bStart) {
+            return A[k - bStart];
+        }
+
+        // Get the middle indexes and middle values of A and B.
+        int aIndex = (aStart + aEnd) / 2, bIndex = (bStart + bEnd) / 2;
+        int aValue = A[aIndex], bValue = B[bIndex];
+        
+        // If k is in the right half of A + B, remove the smaller left half.
+        if (aIndex + bIndex < k) { 
+            if (aValue > bValue) {
+                return getK(A, B, k, aStart, aEnd, bIndex + 1, bEnd);
+            } else {
+                return getK(A, B, k, aIndex + 1, aEnd, bStart, bEnd);
+            }
+        } 
+        // Otherwise, remove the larger right half.
+        else { 
+            if (aValue > bValue) {
+                return getK(A, B, k, aStart, aIndex - 1, bStart, bEnd);
+            } else {
+                return getK(A, B, k, aStart, aEnd, bStart, bIndex - 1);       
+            }
+        }
+        return -1;
+    }
+
+
+double Solution::findMedianSortedArrays(const vector<int> &A, const vector<int> &B) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    int n=A.size(),m=B.size();
+    int tot=(n+m);
+    if(tot%2==1){
+        return getK(A,B,tot/2,0,n-1,0,m-1);
+    }
+    else{
+        int aa=getK(A,B,tot/2,0,n-1,0,m-1);
+        int bb=getK(A,B,(tot-1)/2,0,n-1,0,m-1);
+        return (double)(aa+bb)/2.0;
+    }
 }
 
-
-double median(vector<int>& a, vector<int>& b) {
-	// Write your code here.
-
-	int n=a.size();
-	int m=b.size();
-	int tot=(m+n);
-	if(tot%2==0){
-		int aa=getK(a,0,n-1,b,0,m-1,tot/2);
-		int bb=getK(a,0,n-1,b,0,m-1,(tot-1)/2);
-		return (double)(aa+bb)/2.0;
-	}
-	else{
-		return getK(a,0,n-1,b,0,m-1,tot/2);
-	}
 }
